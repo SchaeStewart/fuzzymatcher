d96e4fb669dd5b00d8ab14eb0fc97129dd1ef7cb
Schae Stewart <schaestewart@gmail.com>
2019-04-26 13:42:19 -0400

    Add command line flag parsing for file name and basic selectors

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -22,5 +23,19 @@ func readJSON(path string) gjson.Result {
 }
 
 func main() {
-	readJSON("durham_pool_permits.json")
+	var file1 string
+	var file1Selector string
+	var file2 string
+	var file2Selector string
+	flag.StringVar(&file1, "file1", "", "the first file")
+	flag.StringVar(&file1Selector, "file1-selector", "", "the first file")
+	flag.StringVar(&file2, "file2", "", "the second file")
+	flag.StringVar(&file2Selector, "file2-selector", "", "TODO: better help messages")
+	flag.Parse()
+
+	file1Res := readJSON(file1)
+	file2Res := readJSON(file2)
+
+	fmt.Println(file1Res.Get(file1Selector))
+	fmt.Println(file2Res.Get(file2Selector))
 }
